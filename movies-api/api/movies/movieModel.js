163f542748f8dac13a8845428cca6f6be62a230d
shaojiayi <sjy030525@163.com>
2024-12-28 10:18:24 +0800

    Added favorites collection and POST /favorites endpoint

--- movies-api/api/movies/movieModel.js
+++ movies-api/api/movies/movieModel.js
@@ -31,6 +31,12 @@ const MovieSchema = new Schema({
   tagline: { type: String }
 });
 
+const FavoriteSchema = new Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+  movieId: { type: Number, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
 MovieSchema.statics.findByMovieDBId = function (id) {
   return this.findOne({ id: id });
 };
